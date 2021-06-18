64551d8f3b94a7983413db7485068afe623e3719
Kalinin_PV <pavelkv96@gmail.com>
2021-06-18 15:02:19 +0300

    feat: Completed tasks

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,23 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val lowcasea = a.toLowerCase()
+        val lowcaseb = b.toLowerCase()
+        val listofa = lowcasea.split("")
+        val listofb = lowcaseb.split("")
+        var i = 1
+        var k = 1
+        var counter = 0
+        while(i <= (listofa.size - 2)) {
+            if(listofb[k]==listofa[i]) {
+                k++
+                counter++
+            }
+            i++
+        }
+
+        if(counter == (listofb.size - 2) && counter != 0) {
+            return "YES"
+        } else return "NO"
     }
 }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,38 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
-    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
+        if(blockB == String::class) {
+            var res = "";
+            for(i in blockA) {
+                if(i is String) {
+                    res += i;
+                }
+            }
+            return res;
+        }
+        if(blockB == Int::class) {
+            var res = 0;
+            for(i in blockA) {
+                if(i is Int) {
+                    res += i;
+                }
+            }
+            return res;
+        }
+        var res = LocalDate.MIN;
+        for(i in blockA) {
+            if(i is LocalDate) {
+                if(i > res) {
+                    res = i;
+                }
+            }
+        }
+        return res.format(DateTimeFormatter.ofPattern("dd.MM.yyy"));
     }
 }
